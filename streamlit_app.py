d212c0753bd4983c41807aca5ce887f444885531
aditya96r <75204276+aditya96r@users.noreply.github.com>
2025-02-13 22:23:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
-st.title('🎈 Portfolio Mangement App')
+st.title('Portfolio Mangement App')
 
 st.info('This app shows your portfolio!')
+
+with st.sidebar:
+  st.head('Profiling') 
