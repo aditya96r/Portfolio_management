ecc239b54384b2ad52830d40b0c511bf02202001
aditya96r <75204276+aditya96r@users.noreply.github.com>
2025-02-13 22:29:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,7 @@ st.title('Portfolio Mangement App')
 st.info('This app shows your portfolio!')
 
 with st.sidebar:
-  st.header('Profiling') 
+  st.header('Investment Risk Tolerance Assessment')
+  How would you react 'if' your portfolio lost 20% 'in' a downturn? = st. selectbox('Sell immediately','Hold and wait',' Buy more because prices are cheaper')
+  
+  
