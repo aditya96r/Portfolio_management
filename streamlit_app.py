974ba75bdc216fbf3dd75ebcdc3b53c6a816c45b
aditya96r <75204276+aditya96r@users.noreply.github.com>
2025-02-13 22:32:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ st.info('This app shows your portfolio!')
 
 with st.sidebar:
   st.header('Investment Risk Tolerance Assessment')
-  'downturn?' = st. selectbox('Downturn',('Sell immediately','Hold and wait',' Buy more because prices are cheaper'))
+  Downturn = st. selectbox('Downturn',('Sell immediately','Hold and wait',' Buy more because prices are cheaper'))
   
   
