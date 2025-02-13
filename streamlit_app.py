518f65bf19625f0364c5c3bc0c0b04e251d8c2b4
aditya96r <75204276+aditya96r@users.noreply.github.com>
2025-02-13 22:38:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.info('This app shows your portfolio!')
 
 with st.sidebar:
   st.header('Investment Risk Tolerance Assessment')
-  Downturn = st. selectbox('Downturn',('Sell immediately','Hold and wait',' Buy more because prices are cheaper'))
+  Downturn = st. selectbox('Downturn',('','Sell immediately','Hold and wait',' Buy more because prices are cheaper'))
   Goal = st. selectbox('Goal',('Saftey','Growth','High Returns'))
   Experience = st. selectbox ('Experience', ('Beginner','Intermediate','Expert'))
   
